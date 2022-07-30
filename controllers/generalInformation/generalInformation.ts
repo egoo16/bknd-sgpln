33ffa92c42d269394d1c9506cd6cf5b9e64d60f8
Geovany Ocaña <egoo.geovany@gmail.com>
2022-07-30 02:29:09 -0600

    General Information API Finished

--- controllers/generalInformation/generalInformation.ts
+++ controllers/generalInformation/generalInformation.ts
@@ -1,31 +1,45 @@
 import { Request, Response } from "express";
 import idea from "../../models/BancoIdeas/idea";
 import sectionBI from "../../models/BancoIdeas/sectionBi";
+import generalInformation from '../../models/BancoIdeas/generalInformation';
 
 export const postGeneralInformation = async (req: Request, res: Response) => {
-  const { body } = req;
+	try {
+		const { body } = req;
 
-  const now = new Date();
-  const milisegundos = now.getMilliseconds();
+		const now = new Date();
+		const milisegundos = now.getMilliseconds();
+	  
+		const nameIdea = "idea-" + milisegundos;
+	  
+		const ideaModel = {
+		  name: nameIdea,
+		};
+	  
+		const ideaInserted = await idea.create(ideaModel);
+	  
+		const sectionModel = {
+		  name: nameIdea,
+		  IdeaId: ideaInserted.codigo,
+		};
+	  
+		const sectionIserted = await sectionBI.create(sectionModel);
+		const informationModel = body;
+		informationModel.sectionBI = sectionIserted.codigo;
+	  
+		const informationIsert = await generalInformation.create(informationModel); 
+	  
+		res.status(201).json({
+		  msg: "ideaIsertada Correctamente",
+		  ideaInserted,
+		  sectionIserted,
+		  informationIsert,
+		});
+	} catch (error) {
+		res.status(500).json({
+			msg: "No se pudo insertrar la Información General",
+			error
+		  });
+	}
 
-  const nameIdea = "idea-" + milisegundos;
-
-  const ideaModel = {
-    name: nameIdea,
-  };
-
-  const ideaInserted = await idea.create(ideaModel);
-
-  const sectionModel = {
-    name: nameIdea,
-    IdeaId: ideaInserted.codigo,
-  }
-
-  const sectionIserted = await sectionBI.create(sectionModel);
-
-  res.json({
-    msg: "ideaIsertada Correctamente",
-    ideaInserted,
-    sectionIserted,
-  });
 };
--- dist/controllers/generalInformation/generalInformation.js
+++ dist/controllers/generalInformation/generalInformation.js
@@ -15,25 +15,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.postGeneralInformation = void 0;
 const idea_1 = __importDefault(require("../../models/BancoIdeas/idea"));
 const sectionBi_1 = __importDefault(require("../../models/BancoIdeas/sectionBi"));
+const generalInformation_1 = __importDefault(require("../../models/BancoIdeas/generalInformation"));
 const postGeneralInformation = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { body } = req;
-    const now = new Date();
-    const milisegundos = now.getMilliseconds();
-    const nameIdea = "idea-" + milisegundos;
-    const ideaModel = {
-        name: nameIdea,
-    };
-    const ideaInserted = yield idea_1.default.create(ideaModel);
-    const sectionModel = {
-        name: nameIdea,
-        IdeaId: ideaInserted.codigo,
-    };
-    const sectionIserted = yield sectionBi_1.default.create(sectionModel);
-    res.json({
-        msg: "ideaIsertada Correctamente",
-        ideaInserted,
-        sectionIserted,
-    });
+    try {
+        const { body } = req;
+        const now = new Date();
+        const milisegundos = now.getMilliseconds();
+        const nameIdea = "idea-" + milisegundos;
+        const ideaModel = {
+            name: nameIdea,
+        };
+        const ideaInserted = yield idea_1.default.create(ideaModel);
+        const sectionModel = {
+            name: nameIdea,
+            IdeaId: ideaInserted.codigo,
+        };
+        const sectionIserted = yield sectionBi_1.default.create(sectionModel);
+        const informationModel = body;
+        informationModel.sectionBI = sectionIserted.codigo;
+        const informationIsert = yield generalInformation_1.default.create(informationModel);
+        res.status(201).json({
+            msg: "ideaIsertada Correctamente",
+            ideaInserted,
+            sectionIserted,
+            informationIsert,
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            msg: "No se pudo insertrar la Información General",
+            error
+        });
+    }
 });
 exports.postGeneralInformation = postGeneralInformation;
 //# sourceMappingURL=generalInformation.js.map
