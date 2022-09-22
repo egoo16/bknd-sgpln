029312175ff2e86bb72bc79920ef6cbadfa9c613
Geovany Ocaña <egoo.geovany@gmail.com>
2022-09-22 01:28:47 -0600

    Conection

---
+++ db/connection.ts
@@ -0,0 +1,22 @@
+// import {Sequelize} from 'sequelize-oracle';
+
+const Sequelize = require('sequelize-oracle');
+
+// import oracledb from 'oracledb'; 
+import oracledb  from 'oracledb'; 
+oracledb.maxRows = 0;
+
+// oracledb.initOracleClient({ libDir: "C:Oracleinstantclient_21_6" });
+let db: any;
+
+// db = new Sequelize('xepdb1', 'SYSTEM', 'database', {
+//     host: 'localhost',
+//     dialect: 'oracle',
+//     // logging: false,
+// });
+db = new Sequelize('osnip', 'PREINV2022', 'F6HRj3T0L3A', {
+    host: '192.168.9.14',
+    dialect: 'oracle',
+    // logging: false,
+});
+export default db;
